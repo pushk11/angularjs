b73f76ed583ec78d96bc1462b668145f4c54540f
Pushpender K. Chauhan <pushpender.kumar@globallogic.com>
2014-10-19 00:48:45 +0530

    just refined few things

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('mainModule',['ngRoute']);
 
-app.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider) {
+app.config(['$routeProvider',function($routeProvider) {
 	  $routeProvider.when('/demo', {
 			templateUrl: 'views/demo.html'
 		})
@@ -13,12 +13,12 @@ app.config(['$routeProvider','$locationProvider',function($routeProvider,$locati
 
 app.controller("demoController1", ['$scope', function($scope) {
 
-	$scope.areaOne = "Area One  for Lovekesh";
-	$scope.arr = ["Tom", "Jony", "Jack", "Sony", "Giny", "Puma"];
+	$scope.areaOne = "Search Keyword";
+	$scope.arr = [{"Name" : "Tom", "Age" : "24"}, {"Name" : "Jony", "Age" : "24"}, {"Name" : "Jack", "Age" : "24"}, {"Name" : "Sony", "Age" : "35"}, {"Name" : "Giny", "Age" : "47"}, {"Name" : "Puma", "Age" : "44"}];
 	
 }]);
 
 app.controller("demoController2", ['$scope', function($scope) {
 	$scope.arr = ["Mohan", "Smith", "Tod", "Lucy", "Alec", "Adidas"];
-	$scope.areaTwo = "Area Two";
+	$scope.areaTwo = "Search Keyword";
 }]);
